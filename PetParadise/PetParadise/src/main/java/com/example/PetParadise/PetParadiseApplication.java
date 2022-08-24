7784be9bf015f8e9cc1a399fb0c9b79bd3711e11
Thasmini <thasmini.thayaparan@noseryoung.ch>
2022-08-24 08:50:58 +0200

    created Gradle project

---
+++ PetParadise/PetParadise/src/main/java/com/example/PetParadise/PetParadiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetParadise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetParadiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetParadiseApplication.class, args);
+	}
+
+}
---
+++ PetParadise/PetParadise/src/test/java/com/example/PetParadise/PetParadiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetParadise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetParadiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
